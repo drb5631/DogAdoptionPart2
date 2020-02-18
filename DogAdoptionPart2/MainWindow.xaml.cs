bf6b3a82eed9fc70ddc56c20bcbb49945fa4b046
dblon <dblon@nextgen.com>
2020-02-18 14:37:39 -0500

    Added appropriate dog information to each selected dog

--- DogAdoptionPart2/MainWindow.xaml.cs
+++ DogAdoptionPart2/MainWindow.xaml.cs
@@ -39,111 +39,111 @@ namespace DogAdoptionPart2
         private void DogBella_Selected(object sender, RoutedEventArgs e)
         {
             dogImageDisplay.Source = new BitmapImage(new Uri("images/bella.jpg", UriKind.Relative));
-            nameTextBox.Text = (string )dogInfo.GetValue(0);
-            bdayTextBox.Text = (string)dogInfo.GetValue(1);
-            breedTextBox.Text = (string)dogInfo.GetValue(2);
-            weightTextBox.Text = (string)dogInfo.GetValue(3);
-            shotsTextBox.Text = (string)dogInfo.GetValue(4);
-            availableTextBox.Text = (string)dogInfo.GetValue(5);
+            nameTextBox.Text = (string)dogInfo.GetValue(0);
+            bdayTextBox.Text = (string)dogInfo.GetValue(10);
+            breedTextBox.Text = (string)dogInfo.GetValue(20);
+            weightTextBox.Text = (string)dogInfo.GetValue(30);
+            shotsTextBox.Text = (string)dogInfo.GetValue(40);
+            availableTextBox.Text = (string)dogInfo.GetValue(50);
         }
 
         private void DogCharlie_Selected(object sender, RoutedEventArgs e)
         {
             dogImageDisplay.Source = new BitmapImage(new Uri("images/charlie.jpg", UriKind.Relative));
-            nameTextBox.Text = (string)dogInfo.GetValue(0);
-            bdayTextBox.Text = (string)dogInfo.GetValue(1);
-            breedTextBox.Text = (string)dogInfo.GetValue(2);
-            weightTextBox.Text = (string)dogInfo.GetValue(3);
-            shotsTextBox.Text = (string)dogInfo.GetValue(4);
-            availableTextBox.Text = (string)dogInfo.GetValue(5);
+            nameTextBox.Text = (string)dogInfo.GetValue(1);
+            bdayTextBox.Text = (string)dogInfo.GetValue(11);
+            breedTextBox.Text = (string)dogInfo.GetValue(21);
+            weightTextBox.Text = (string)dogInfo.GetValue(31);
+            shotsTextBox.Text = (string)dogInfo.GetValue(41);
+            availableTextBox.Text = (string)dogInfo.GetValue(51);
         }
 
         private void DogLuna_Selected(object sender, RoutedEventArgs e)
         {
             dogImageDisplay.Source = new BitmapImage(new Uri("images/luna.jpg", UriKind.Relative));
-            nameTextBox.Text = (string)dogInfo.GetValue(0);
-            bdayTextBox.Text = (string)dogInfo.GetValue(1);
-            breedTextBox.Text = (string)dogInfo.GetValue(2);
-            weightTextBox.Text = (string)dogInfo.GetValue(3);
-            shotsTextBox.Text = (string)dogInfo.GetValue(4);
-            availableTextBox.Text = (string)dogInfo.GetValue(5);
+            nameTextBox.Text = (string)dogInfo.GetValue(2);
+            bdayTextBox.Text = (string)dogInfo.GetValue(12);
+            breedTextBox.Text = (string)dogInfo.GetValue(22);
+            weightTextBox.Text = (string)dogInfo.GetValue(32);
+            shotsTextBox.Text = (string)dogInfo.GetValue(42);
+            availableTextBox.Text = (string)dogInfo.GetValue(52);
         }
 
         private void DogLucy_Selected(object sender, RoutedEventArgs e)
         {
             dogImageDisplay.Source = new BitmapImage(new Uri("images/lucy.jpg", UriKind.Relative));
-            nameTextBox.Text = (string)dogInfo.GetValue(0);
-            bdayTextBox.Text = (string)dogInfo.GetValue(1);
-            breedTextBox.Text = (string)dogInfo.GetValue(2);
-            weightTextBox.Text = (string)dogInfo.GetValue(3);
-            shotsTextBox.Text = (string)dogInfo.GetValue(4);
-            availableTextBox.Text = (string)dogInfo.GetValue(5);
+            nameTextBox.Text = (string)dogInfo.GetValue(3);
+            bdayTextBox.Text = (string)dogInfo.GetValue(13);
+            breedTextBox.Text = (string)dogInfo.GetValue(23);
+            weightTextBox.Text = (string)dogInfo.GetValue(33);
+            shotsTextBox.Text = (string)dogInfo.GetValue(43);
+            availableTextBox.Text = (string)dogInfo.GetValue(53);
         }
 
         private void DogMax_Selected(object sender, RoutedEventArgs e)
         {
             dogImageDisplay.Source = new BitmapImage(new Uri("images/max.jpg", UriKind.Relative));
-            nameTextBox.Text = (string)dogInfo.GetValue(0);
-            bdayTextBox.Text = (string)dogInfo.GetValue(1);
-            breedTextBox.Text = (string)dogInfo.GetValue(2);
-            weightTextBox.Text = (string)dogInfo.GetValue(3);
-            shotsTextBox.Text = (string)dogInfo.GetValue(4);
-            availableTextBox.Text = (string)dogInfo.GetValue(5);
+            nameTextBox.Text = (string)dogInfo.GetValue(4);
+            bdayTextBox.Text = (string)dogInfo.GetValue(14);
+            breedTextBox.Text = (string)dogInfo.GetValue(24);
+            weightTextBox.Text = (string)dogInfo.GetValue(34);
+            shotsTextBox.Text = (string)dogInfo.GetValue(44);
+            availableTextBox.Text = (string)dogInfo.GetValue(54);
         }
 
         private void DogBailey_Selected(object sender, RoutedEventArgs e)
         {
             dogImageDisplay.Source = new BitmapImage(new Uri("images/bailey.jpg", UriKind.Relative));
-            nameTextBox.Text = (string)dogInfo.GetValue(0);
-            bdayTextBox.Text = (string)dogInfo.GetValue(1);
-            breedTextBox.Text = (string)dogInfo.GetValue(2);
-            weightTextBox.Text = (string)dogInfo.GetValue(3);
-            shotsTextBox.Text = (string)dogInfo.GetValue(4);
-            availableTextBox.Text = (string)dogInfo.GetValue(5);
+            nameTextBox.Text = (string)dogInfo.GetValue(5);
+            bdayTextBox.Text = (string)dogInfo.GetValue(15);
+            breedTextBox.Text = (string)dogInfo.GetValue(25);
+            weightTextBox.Text = (string)dogInfo.GetValue(35);
+            shotsTextBox.Text = (string)dogInfo.GetValue(45);
+            availableTextBox.Text = (string)dogInfo.GetValue(55);
         }
 
         private void DogCooper_Selected(object sender, RoutedEventArgs e)
         {
             dogImageDisplay.Source = new BitmapImage(new Uri("images/cooper.jpg", UriKind.Relative));
-            nameTextBox.Text = (string)dogInfo.GetValue(0);
-            bdayTextBox.Text = (string)dogInfo.GetValue(1);
-            breedTextBox.Text = (string)dogInfo.GetValue(2);
-            weightTextBox.Text = (string)dogInfo.GetValue(3);
-            shotsTextBox.Text = (string)dogInfo.GetValue(4);
-            availableTextBox.Text = (string)dogInfo.GetValue(5);
+            nameTextBox.Text = (string)dogInfo.GetValue(6);
+            bdayTextBox.Text = (string)dogInfo.GetValue(16);
+            breedTextBox.Text = (string)dogInfo.GetValue(26);
+            weightTextBox.Text = (string)dogInfo.GetValue(36);
+            shotsTextBox.Text = (string)dogInfo.GetValue(46);
+            availableTextBox.Text = (string)dogInfo.GetValue(56);
         }
 
         private void DogDaisy_Selected(object sender, RoutedEventArgs e)
         {
             dogImageDisplay.Source = new BitmapImage(new Uri("images/daisy.jpg", UriKind.Relative));
-            nameTextBox.Text = (string)dogInfo.GetValue(0);
-            bdayTextBox.Text = (string)dogInfo.GetValue(1);
-            breedTextBox.Text = (string)dogInfo.GetValue(2);
-            weightTextBox.Text = (string)dogInfo.GetValue(3);
-            shotsTextBox.Text = (string)dogInfo.GetValue(4);
-            availableTextBox.Text = (string)dogInfo.GetValue(5);
+            nameTextBox.Text = (string)dogInfo.GetValue(7);
+            bdayTextBox.Text = (string)dogInfo.GetValue(17);
+            breedTextBox.Text = (string)dogInfo.GetValue(27);
+            weightTextBox.Text = (string)dogInfo.GetValue(37);
+            shotsTextBox.Text = (string)dogInfo.GetValue(47);
+            availableTextBox.Text = (string)dogInfo.GetValue(57);
         }
 
         private void DogHexi_Selected(object sender, RoutedEventArgs e)
         {
             dogImageDisplay.Source = new BitmapImage(new Uri("images/hexi.jpg", UriKind.Relative));
-            nameTextBox.Text = (string)dogInfo.GetValue(0);
-            bdayTextBox.Text = (string)dogInfo.GetValue(1);
-            breedTextBox.Text = (string)dogInfo.GetValue(2);
-            weightTextBox.Text = (string)dogInfo.GetValue(3);
-            shotsTextBox.Text = (string)dogInfo.GetValue(4);
-            availableTextBox.Text = (string)dogInfo.GetValue(5);
+            nameTextBox.Text = (string)dogInfo.GetValue(8);
+            bdayTextBox.Text = (string)dogInfo.GetValue(18);
+            breedTextBox.Text = (string)dogInfo.GetValue(28);
+            weightTextBox.Text = (string)dogInfo.GetValue(38);
+            shotsTextBox.Text = (string)dogInfo.GetValue(48);
+            availableTextBox.Text = (string)dogInfo.GetValue(58);
         }
 
         private void DogBernadette_Selected(object sender, RoutedEventArgs e)
         {
             dogImageDisplay.Source = new BitmapImage(new Uri("images/bernadette.jpg", UriKind.Relative));
-            nameTextBox.Text = (string)dogInfo.GetValue(0);
-            bdayTextBox.Text = (string)dogInfo.GetValue(1);
-            breedTextBox.Text = (string)dogInfo.GetValue(2);
-            weightTextBox.Text = (string)dogInfo.GetValue(3);
-            shotsTextBox.Text = (string)dogInfo.GetValue(4);
-            availableTextBox.Text = (string)dogInfo.GetValue(5);
+            nameTextBox.Text = (string)dogInfo.GetValue(9);
+            bdayTextBox.Text = (string)dogInfo.GetValue(19);
+            breedTextBox.Text = (string)dogInfo.GetValue(29);
+            weightTextBox.Text = (string)dogInfo.GetValue(39);
+            shotsTextBox.Text = (string)dogInfo.GetValue(49);
+            availableTextBox.Text = (string)dogInfo.GetValue(59);
         }
 
         private void ExitButton_Click(object sender, RoutedEventArgs e)
