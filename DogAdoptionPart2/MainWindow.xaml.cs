a99009a7acae974beb85123c592efffd174155f1
dblon <dblon@nextgen.com>
2020-02-18 13:48:17 -0500

    Added image change functionality to dog choices
    Added application exit functionality

--- DogAdoptionPart2/MainWindow.xaml.cs
+++ DogAdoptionPart2/MainWindow.xaml.cs
@@ -32,7 +32,57 @@ namespace DogAdoptionPart2
 
         private void DogBella_Selected(object sender, RoutedEventArgs e)
         {
+            dogImageDisplay.Source = new BitmapImage(new Uri("images/bella.jpg", UriKind.Relative));
+        }
 
+        private void DogCharlie_Selected(object sender, RoutedEventArgs e)
+        {
+            dogImageDisplay.Source = new BitmapImage(new Uri("images/charlie.jpg", UriKind.Relative));
+        }
+
+        private void DogLuna_Selected(object sender, RoutedEventArgs e)
+        {
+            dogImageDisplay.Source = new BitmapImage(new Uri("images/luna.jpg", UriKind.Relative));
+        }
+
+        private void DogLucy_Selected(object sender, RoutedEventArgs e)
+        {
+            dogImageDisplay.Source = new BitmapImage(new Uri("images/lucy.jpg", UriKind.Relative));
+        }
+
+        private void DogMax_Selected(object sender, RoutedEventArgs e)
+        {
+            dogImageDisplay.Source = new BitmapImage(new Uri("images/max.jpg", UriKind.Relative));
+        }
+
+        private void DogBailey_Selected(object sender, RoutedEventArgs e)
+        {
+            dogImageDisplay.Source = new BitmapImage(new Uri("images/bailey.jpg", UriKind.Relative));
+        }
+
+        private void DogCooper_Selected(object sender, RoutedEventArgs e)
+        {
+            dogImageDisplay.Source = new BitmapImage(new Uri("images/cooper.jpg", UriKind.Relative));
+        }
+
+        private void DogDaisy_Selected(object sender, RoutedEventArgs e)
+        {
+            dogImageDisplay.Source = new BitmapImage(new Uri("images/daisy.jpg", UriKind.Relative));
+        }
+
+        private void DogHexi_Selected(object sender, RoutedEventArgs e)
+        {
+            dogImageDisplay.Source = new BitmapImage(new Uri("images/hexi.jpg", UriKind.Relative));
+        }
+
+        private void DogBernadette_Selected(object sender, RoutedEventArgs e)
+        {
+            dogImageDisplay.Source = new BitmapImage(new Uri("images/bernadette.jpg", UriKind.Relative));
+        }
+
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
         }
     }
 }
